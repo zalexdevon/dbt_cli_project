39294e89aec006784b6958febf135cb24c6fd2b8
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-09 17:09:02 +0700

    c

--- upload_data/utils/limit_data.py
+++ upload_data/utils/limit_data.py
@@ -2,15 +2,15 @@ import pandas as pd
 import sys
 
 # Get param
-csv_file = sys.argv[1]
-csv_file_limited = sys.argv[2]
+source_file = sys.argv[1]
+source_file_limited = sys.argv[2]
 limit_num_row = int(sys.argv[3])
 
 # Read file
-df = pd.read_csv(csv_file)
+df = pd.read_parquet(source_file)
 
 # Take 10000 rows
 df = df.head(limit_num_row)
 
 # Save file
-df.to_csv(csv_file_limited, index=False)
+df.to_parquet(source_file_limited, engine="pyarrow")
