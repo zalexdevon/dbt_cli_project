078ef71da664e0a914db1be508815584e1f26567
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-10 09:54:52 +0700

    c

--- upload_data/yellow/sql/complete_staging_table.sql
+++ upload_data/yellow/sql/complete_staging_table.sql
@@ -4,4 +4,7 @@ unique_row_id = md5(
     COALESCE(CAST(VendorID AS text), '') ||
     COALESCE(CAST(tpep_pickup_datetime AS text), '')
 ),
-filename = '{{source_file}}';
+filename = '{{source_file}}', 
+passenger_count = CAST(passenger_count AS integer), 
+payment_type = CAST(payment_type AS integer), 
+;
